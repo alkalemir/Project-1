3accdfe01a828f0c9118b3510ede0f929f8bf84f
Emir <alkalemir@gmail.com>
2022-05-30 17:26:32 +0300

    Initial Commit

---
+++ project-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project-1
+//
+//  Created by Emir Alkal on 30.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
